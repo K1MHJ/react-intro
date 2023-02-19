cf623da13f2c7a88827e2ea9ae8b0efb9d714887
K1MHJ <hojokimu@gmail.com>
2023-02-19 23:42:26 +0900

    websocket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+const WS_URL = 'ws://127.0.0.1:8000'
 
 function App() {
+  // Global var
+  var response
+  // Connection to WS
+  const connect = () => {
+    return new Promise(function (resolve, reject) {
+      var server = new WebSocket(WS_URL)
+      server.onopen = function () {
+        resolve(server)
+      }
+      server.onerror = function (err) {
+        console.log(err)
+        reject(err)
+      }
+    })
+  }
+  const request = async (data) => {
+    var request = {
+      id: 'react-intro',
+      data: data,
+    }
+    connect()
+      .then((server) => {
+        server.send(JSON.stringify(request))
+        server.onmessage = ({ data }) => {
+          response = JSON.parse(data) // save result
+          console.log(response)
+        }
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+  }
+  const clickme = () => {
+    request('run')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={clickme}>Click</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
